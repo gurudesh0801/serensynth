4fd0868fa6202ef594c6e64f22c7a27e3499e888
gurudesh@0801 <gurudesh2204@gmail.com>
2025-04-02 20:26:15 +0530

    add contact and terms & condition page

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,137 +1,97 @@
 import React from "react";
-import "./ContactUs.css";
 import {
   FaPhone,
   FaEnvelope,
   FaMapMarkerAlt,
-  FaTwitter,
   FaInstagram,
+  FaTwitter,
   FaDiscord,
 } from "react-icons/fa";
+import "./ContactUs.css";
 
 const ContactUs = () => {
   return (
-    <>
-      <div className="contactContainer">
-        <h1 className="contactHeader">Contact Us</h1>
-        <div className="mainContainer">
-          <div className="leftCont">
-            <h1>Contact Information</h1>
-            <p>Say something to start a live chat!</p>
-            <div className="network">
-              <p>
-                <span>
-                  <FaPhone />
-                </span>
-                &nbsp; +91 8062179313
-              </p>
-              <p>
-                <span>
-                  <FaEnvelope />
-                </span>
-                &nbsp; assist@serensynthlabs.com
-              </p>
-              <p>
-                <span>
-                  <FaMapMarkerAlt />
-                </span>
-                &nbsp; 301,BALEWADI PLAZA,NEAR MITCON INSTITUTE,BALEWADI, PUNE
-                CITY, MAHARASHTRA, PIN: 411045
-                <br />
-                <br />
-                Operational Address : Flat No 18, 5th Floor, Mahalaxmi Down
-                Town, Savarkar Nagar, Gangapur Road, Udhoji Maratha Boarding,
-                Nashik, Nashik, Maharashtra, India, 422007
-              </p>
+    <div className="contact-container">
+      <h2 className="contact-title">Contact Us</h2>
+      <div className="contact-box">
+        {/* Left Side - Contact Info */}
+        <div className="contact-info">
+          <h3>Contact Information</h3>
+          <p>Say something to start a live chat!</p>
+          <div className="contact-detail">
+            <FaPhone className="contact-icon" />
+            <span>+91 8062179313</span>
+          </div>
+          <div className="contact-detail">
+            <FaEnvelope className="contact-icon" />
+            <span>assist@serensynthlabs.com</span>
+          </div>
+          <div className="contact-detail">
+            <FaMapMarkerAlt className="contact-icon" />
+            <span>
+              301,BALEWADI PLAZA,NEAR MITCON INSTITUTE,BALEWADI, PUNE CITY,
+              MAHARASHTRA, PIN: 411045
+            </span>
+          </div>
+          <div className="contact-detail">
+            <span>
+              Operational Address : Flat No 18, 5th Floor, Mahalaxmi Down Town,
+              Savarkar Nagar, Gangapur Road, Udhoji Maratha Boarding, Nashik,
+              Nashik, Maharashtra, India, 422007
+            </span>
+          </div>
+          <div className="contact-social">
+            <FaInstagram className="social-icon" />
+            <FaTwitter className="social-icon" />
+            <FaDiscord className="social-icon" />
+          </div>
+        </div>
 
-              <div className="social-icons">
-                <span>
-                  <FaTwitter />
-                </span>
-                <span>
-                  <FaInstagram />
-                </span>
-                <span>
-                  <FaDiscord />
-                </span>
-              </div>
+        {/* Right Side - Contact Form */}
+        <div className="contact-form">
+          <div className="form-row">
+            <div className="form-group">
+              <label>First Name</label>
+              <input type="text" placeholder="First Name" />
+            </div>
+            <div className="form-group">
+              <label>Last Name</label>
+              <input type="text" placeholder="Last Name" />
             </div>
           </div>
-          <div className="rightCont">
-            <form action="#">
-              <div className="formContainer">
-                <div className="firstContainer">
-                  <div>
-                    <label htmlFor="FirstName">First Name</label>
-                    <input
-                      type="text"
-                      name="firstName"
-                      id="firstName"
-                      placeholder="Sakshi"
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="lastName">Last Name</label>
-                    <input
-                      type="text"
-                      name="lastName"
-                      id="lastName"
-                      placeholder="ABC"
-                    />
-                  </div>
-                </div>
-                <div className="secondContainer">
-                  <div>
-                    <label htmlFor="emil">Email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="abc@gmail.com"
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="PhoneNo">Phone Number</label>
-                    <input
-                      type="text"
-                      name="phoneNo"
-                      id="phoneNo"
-                      placeholder="1234567890"
-                    />
-                  </div>
-                </div>
-                <div className="selectDiv">
-                  <h5>Select Subject?</h5>
-                  <div className="options">
-                    <input type="radio" name="option" />
-                    <input type="radio" name="option" />
 
-                    <label htmlFor="other">
-                      <input type="radio" name="option" />
-                      &nbsp; other
-                    </label>
-                  </div>
-                </div>
-                <div className="thirdContainer">
-                  <div>
-                    <label htmlFor="Message">Message</label>
-                    <input
-                      type="text"
-                      name="message"
-                      id="message"
-                      placeholder="Write a Message"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="contact-btn-container">
-                <button className="contact-btn">Submit</button>
-              </div>
-            </form>
+          <div className="form-row">
+            <div className="form-group">
+              <label>Email</label>
+              <input type="email" placeholder="abc@123gmail.com" />
+            </div>
+            <div className="form-group">
+              <label>Phone Number</label>
+              <input type="tel" placeholder="1234567890" />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label>Select Subject?</label>
+            <div className="radio-group">
+              <input type="radio" name="subject" />
+              <input type="radio" name="subject" />
+              <input type="radio" name="subject" />
+              <span>Other</span>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label>Message</label>
+            <textarea placeholder="Write your message.."></textarea>
+          </div>
+
+          <div class="contact-btn-container">
+            <button class="contact-btn">Send Message</button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
